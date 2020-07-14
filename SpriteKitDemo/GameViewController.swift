4f68dfd8fbd22f7a122dd60a838af2c012d51937
Вячеслав Титов <vcslvttv@gmail.com>
2020-07-14 19:19:47 +0500

    Demo project

--- SpriteKitDemo/GameViewController.swift
+++ SpriteKitDemo/GameViewController.swift
@@ -20,7 +20,7 @@ class GameViewController: UIViewController {
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
+                scene.size = self.view.bounds.size
                 // Present the scene
                 view.presentScene(scene)
             }
@@ -37,11 +37,7 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .portrait
     }
 
     override var prefersStatusBarHidden: Bool {
